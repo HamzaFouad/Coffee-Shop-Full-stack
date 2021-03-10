879b89b47d36532164b16221fb633490c6873a0e
HamzaFouad <HamzaMoFouad@gmail.com>
2021-03-10 20:05:57 +0200

    Add Endpoints

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,110 +1,197 @@
 import os
-from flask import Flask, request, jsonify, abort
+from flask import Flask, request, jsonify, abort, redirect, url_for
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
+from werkzeug.exceptions import Unauthorized
 
 from .database.models import db_drop_and_create_all, setup_db, Drink
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
 setup_db(app)
-CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+class Color:
+  PURPLE = '\033[95m'
+  CYAN = '\033[96m'
+  DARKCYAN = '\033[36m'
+  BLUE = '\033[94m'
+  GREEN = '\033[92m'
+  YELLOW = '\033[93m'
+  RED = '\033[91m'
+  BOLD = '\033[1m'
+  UNDERLINE = '\033[4m'
+  END = '\033[0m'
+
+
+OK = 200
+CREATED = 201
+BAD_REQUEST = 400
+NOT_FOUND = 404
+NOT_PROCESSABLE = 422
+UNAUTHORIZED = 401
 
 '''
 @TODO uncomment the following line to initialize the datbase
 !! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 '''
+
 # db_drop_and_create_all()
 
+# CORS Headers
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+  
+
 ## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/', methods=['GET'])
+def home():
+  print(f'{Color.GREEN}Application restarted!{Color.END}')
+  print(f'{Color.GREEN}And running on http://127.0.0.1:5000/{Color.END}')
+  return 'Empty HOME Directory'
+
+@app.route('/drinks', methods=['GET'])
+def get_drinks():
+  drinks = Drink.query.all()
+
+  return jsonify({
+      'success': True,
+      'drinks': [drink.short() for drink in drinks]
+  }), OK
 
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(jwt):
+  drinks = Drink.query.all()
+
+  return jsonify({
+    'drinks': [drink.short() for drink in drinks],
+    'success': True
+  }), OK
 
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def post_drink(jwt):
+  body = request.get_json()
+  title = body.get('title', None)
+  recipe = body.get('recipe', None)
+
+  if None in (title, recipe):
+    print(f'{Color.RED}Failed to POST a drink: There is a None Value.\ntitle:{title}, recipe:{recipe}{Color.END}')
+    abort(BAD_REQUEST)
+
+  try:
+    created_drink = Drink(
+      title = title,
+      recipe = json.dumps(recipe)
+    )
+    created_drink.insert()
+    return jsonify({
+      'success': True,
+      'drinks': [created_drink.long()]
+    }), OK
+  except Exception as e:
+    print(f'{Color.RED}EXCEPTION ERROR WHEN POSTING:\n{e}{Color.END}')
+    abort(BAD_REQUEST)
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def modify_drink(jwt, id):
+  '''
+  Modify either the title of the drink, recipe or both.
+  '''
+  print('\n\n\n\n\n\n\n')
+  drink = Drink.query.get(id)
+  if drink is None:
+    print(f'{Color.RED}Failed to PATCH the drink: Error "drink is None"{Color.END}')
+    abort(NOT_FOUND)
+  
+  body = request.get_json()
 
+  if body is None:
+    print(f'{Color.RED}Failed to PATCH the drink: Error "body is None"{Color.END}')
+    abort(BAD_REQUEST)
+  
+  title = body.get('title', None)
+  recipe = body.get('recipe', None)
+  
+  if title is None:
+    title = drink.title
+  
+  if recipe is None:
+    recipe = json.loads(drink.recipe)
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
+  if None in (title, recipe):
+    print(f'{Color.RED}Failed to PATCH the drink: There is a None Value.\ntitle:{title}, recipe:{recipe}"{Color.END}')
+    abort(BAD_REQUEST)
+  
+  try:
+    drink.title = title
+    drink.recipe = json.dumps(recipe)
+    drink.update()
+    return jsonify({
+      'success': True,
+      'drinks': [drink.long()]
+    }), OK
+  except Exception as e:
+    print(f'{Color.RED}EXCEPTION ERROR WHEN PATCHING:\n{e}{Color.END}')
+    abort(BAD_REQUEST)
+  
+
+@app.route('/drinks/<int:id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(jwt, id):
+  drink = Drink.query.get(id)
+  
+  if drink is None:
+    print(f'{Color.RED}Failed to DELETE the drink: Error "drink is None"{Color.END}')
+    abort(NOT_FOUND)
+
+  try:
+    drink.delete()
+    return jsonify({
+      'success': True,
+      'delete': drink.id
+    }), OK
+  except Exception as e:
+    print(f'{Color.RED}EXCEPTION ERROR WHEN DELETING:\n{e}{Color.END}')
+    abort(BAD_REQUEST)
 
 
 ## Error Handling
-'''
-Example error handling for unprocessable entity
-'''
-@app.errorhandler(422)
+@app.errorhandler(BAD_REQUEST)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": BAD_REQUEST,
+        "message": "BAD REQUEST"
+    }), BAD_REQUEST
+
+@app.errorhandler(NOT_FOUND)
+def not_found(error):
+  return jsonify({
+    "success": False, 
+    "error": NOT_FOUND,
+    "message": "RESOURCE NOT FOUND"
+    }), NOT_FOUND
+
+@app.errorhandler(NOT_PROCESSABLE)
 def unprocessable(error):
     return jsonify({
-                    "success": False, 
-                    "error": 422,
-                    "message": "unprocessable"
-                    }), 422
+        "success": False,
+        "error": NOT_PROCESSABLE,
+        "message": "NOT PROCESSABLE"
+    }), NOT_PROCESSABLE
+    
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
+# Manager Token: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjUyWjE0eUNFaGdoVzFnV1BuclVkdCJ9.eyJpc3MiOiJodHRwczovL2F1dGh0dXRvcmlhbC5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjA0NzA2YzkwZGM4NWIwMDY4MjQ5ZmY4IiwiYXVkIjoiaW1hZ2UiLCJpYXQiOjE2MTUzMzgwNzMsImV4cCI6MTYxNTM3NDA3MywiYXpwIjoiY0VNNmVrazhiUXByUTVGVEJEZnRpVndicWljOUZZOUQiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImRlbGV0ZTpkcmlua3MiLCJnZXQ6ZHJpbmtzLWRldGFpbCIsInBhdGNoOmRyaW5rcyIsInBvc3Q6ZHJpbmtzIl19.g6IV7Y8S6RvrCTxH1qK5oEaaXghXC8uYwP-Nq0ZkMWYEjtVajAPR7gZa2BYhPzjZlGY8JXj1QRZHCzdCF1k_t_pdh0qtOoCbvPKE79PUKSBlR96mucvE01rd3UL6lO7kCasmPlHb6H8wP8PevRhvSTuceud4yo0zu81xdMbIDOZL-Xg20d_TaNJ0fph0xnmonMJBBqykRYiemAbEAbx_P6MEnGaIcieuSfzXj85Viy5lqhjT9a2eH12FhVVtXQIlQz3FDQkrYXLqmfjAKy3qbCDXkRDdiQLJuK_P1VMHcosmwY0WzssaqClgrVnfAjuW7alamP3vC7Bjt3bmAeXLLQ
+# Barista Token: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjUyWjE0eUNFaGdoVzFnV1BuclVkdCJ9.eyJpc3MiOiJodHRwczovL2F1dGh0dXRvcmlhbC5ldS5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjA0ODUxNzkwZGM4NWIwMDY4MjU1OWVhIiwiYXVkIjoiaW1hZ2UiLCJpYXQiOjE2MTUzNTI5NTcsImV4cCI6MTYxNTM4ODk1NywiYXpwIjoiY0VNNmVrazhiUXByUTVGVEJEZnRpVndicWljOUZZOUQiLCJzY29wZSI6IiIsInBlcm1pc3Npb25zIjpbImdldDpkcmlua3MtZGV0YWlsIl19.dRvUHYWOWWvQtMWHwvbHK1OSkIt2FEL2vrhqkR_fBO7sLL0u-s6IzK1rstpups3XE05d2V3rzUs_3bXYOcymX7a7kvm9_10vxpHMZ6lHFrYCuy2hqFCkUAs0hWV0v4zsNvTisPkM4skpzzGmBXOSwkTKTW5HWnsAPHxLCpFYb9BBMPrsox54VZyBptgZ7DYqSB4C93w5ydgcksC4sjziD6-x3bVhC52jcswr7hzoR2gbkKJh76aifKT8yxg4Ozl7TiFMME23MDO72RmLgLPGxLfLnN9kZX3PDsU9AsEmmwuiwKG1t-04PF3VgKHTFUb2hLFNvTcuAWAQHsJY_cJc0A
+# https://authtutorial.eu.auth0.com/authorize?audience=image&response_type=token&client_id=cEM6ekk8bQprQ5FTBDftiVwbqic9FY9D&redirect_uri=https://localhost.com:8100/
